4d0758068de18aec7ca3453a4ef093cd5e648544
ShadowSquad-7 <pogodina12051833@gmail.com>
2025-03-29 05:05:36 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
 
-app=FastAPI()
+app = FastAPI()
 
-#хуй пизда сковорода
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("MainPage.html", {"request": request})
