080d733aeafe3d360898306c741ffd59c0d49499
ShadowSquad-7 <pogodina12051833@gmail.com>
2025-04-06 13:24:48 +0300

    Update model.py

--- app/users/model.py
+++ app/users/model.py
@@ -4,6 +4,6 @@ from app.database import DATA_BASE
 class User(DATA_BASE):
     __tablename__='users'
     
-    id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, nullable=False, unique=True, index=True)
     pswrd = Column(String, nullable=False)
