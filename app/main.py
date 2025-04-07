471f332546909f24f1135e4cd076f89b4fe22e94
ShadowSquad-7 <pogodina12051833@gmail.com>
2025-04-07 13:35:03 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Depends
-from pydantic import BaseModel
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 
@@ -9,8 +9,10 @@ from app.users import routers as user_rout
 app = FastAPI()
 app.include_router(user_rout.router)
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
+
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("MainPage.html", {"request": request})
+async def read_main(request: Request):
+    return templates.TemplateResponse("main_page.html", {"request": request, "user": None})
