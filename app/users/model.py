49c9572743621c6325556b760be20249428a4c17
ShadowSquad-7 <pogodina12051833@gmail.com>
2025-04-12 11:52:26 +0300

    Update model.py

--- app/users/model.py
+++ app/users/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 from app.database import DATA_BASE
 
 class User(DATA_BASE):
@@ -7,4 +7,8 @@ class User(DATA_BASE):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, nullable=False, unique=True, index=True)
     pswrd = Column(String, nullable=False)
-    balance = Column(Integer, default=10000)
+    balance = Column(Float, default=10000.0)
+    usd_balance = Column(Float, default=0.00)
+    btc_balance = Column(Float, default=0.00)
+    eur_balance = Column(Float, default=0.00)
+    cny_balance = Column(Float, default=0.00)
