e1dadcd9dfcf023e4b0b9f5a7ee93d0e872598cb
Gertvaaborta <lololo822228822228822228@gmail.com>
2025-04-23 13:34:26 +0300

    Update parsersbitok.py

--- app/parcers/parsersbitok.py
+++ app/parcers/parsersbitok.py
@@ -7,27 +7,31 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Настройка драйвера
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 try:
     driver.get("https://www.binance.com/ru/markets")
 
-    while True:  # Бесконечный цикл для парсинга динамических данных
+    while True:
         try:
-            # Каждый раз ищем элемент заново
-            price_element1 = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located(
+            # Получаем ВСЕ элементы с ценами (не только первый)
+            price_elements = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located(
                     (By.CSS_SELECTOR, 'div[data-area="right"].layout-ellipsis.t-Body2.w-\\[148px\\].items-center')
                 )
             )
-            print(f"Текущая цена: {price_element1.text}")
+            desired_index = 0  
+
+            if len(price_elements) > desired_index:
+                print(f"Цена валюты: {price_elements[desired_index].text}")
+            else:
+                print(f"Элемент с индексом {desired_index} не найден")
 
         except Exception as e:
-            print(f"Ошибка при получении элемента: {str(e)}")
+            print(f"Ошибка при получении элементов: {str(e)}")
 
-        time.sleep(3)  # Пауза между обновлениями
+        time.sleep(3)
 
 except KeyboardInterrupt:
     print("Парсинг остановлен пользователем")
