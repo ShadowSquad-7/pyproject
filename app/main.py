ce19f9e5f345bbb50f416c9b1a8ea96a26a2c7c8
ShadowSquad-7 <pogodina12051833@gmail.com>
2025-05-27 07:48:39 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 
-from app.users import routers as user_rout
+from app.curr.services import lifespan
+from app.users import routers as user_rout, model
+from app.users.security import get_current_user
+from app.curr.routers import router as curr_router
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(user_rout.router)
+app.include_router(curr_router)
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_main(request: Request):
-    return templates.TemplateResponse("main_page.html", {"request": request, "user": None})
+async def read_main(request: Request, user: model.User | None = Depends(get_current_user)):
+    return templates.TemplateResponse("main_page.html", {
+        "request": request,
+        "user" : user,
+    })
